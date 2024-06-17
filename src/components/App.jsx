19f0d7f58c16700b59a0ca9126f740c1869fe086
lotickles <148078790+lotickles@users.noreply.github.com>
2024-06-17 21:02:07 +0800

    HW1 updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
 import { Statistics } from './Statistics/Statistics';
 import data from '../data/data.json';
-// import { FriendList } from './FriendList/FriendList';
-// import friends from '../data/friends.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from '../data/friends.json';
 // import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 // import transactions from '../data/transactions.json';
 
@@ -18,8 +18,8 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload Stats" stats={data} />
-      {/* <FriendList friends={friends} />
-      <TransactionHistory items={transactions} /> */}
+      <FriendList friends={friends} />
+      {/* <TransactionHistory items={transactions} /> */}
     </>
   );
 };
